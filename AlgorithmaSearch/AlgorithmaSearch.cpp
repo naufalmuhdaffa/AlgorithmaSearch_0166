7f05d686ac14b85dcab9b92586b4fa588e223d3f
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-04-04 10:37:14 +0700

    menambahkan variabel global n

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int arr[20]; // Array to be searched
+int arr[20];    // Array to be searched
+int n;          // Number of elements in the array
+
 
 int main()
 {
