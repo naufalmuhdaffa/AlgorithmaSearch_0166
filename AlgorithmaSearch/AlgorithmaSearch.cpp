121b0c04d5e7e77bc71a050aedc255279ab5b66d
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-04-04 10:59:56 +0700

    menambahkan statement if

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -40,6 +40,11 @@ void LinearSearch() {
         while (i < n)//step 3
         {
             ctr++;
+            if (arr[i] == item) {
+                cout << "\n" << item << " found at position " << (i + 1) << endl; //step 6
+                break;
+            }
+            i++; //step 4
         }
     } while ((ch == 'y') || (ch == 'Y'));
 }
