555bdaa8da9e4e4bc788bce9080457f955633dc9
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-04-04 10:39:55 +0700

    menambahkan perulangan while

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -6,7 +6,10 @@ int n;          // Number of elements in the array
 int i;          // Index of array element
 
 void input() {
-
+    while (true) {
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+    }
 }
 
 int main()
